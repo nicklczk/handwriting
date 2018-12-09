981045ad636ccd63bfd051f09c6f0e235d0cfa97
Sean Rice <seane.rice@gmail.com>
2018-12-09 13:26:14 -0500

    Add more cmd arguments, model load/saving, and statistics

--- Handwriting_Recognition/Service/Net/classify_nn.py
+++ Handwriting_Recognition/Service/Net/classify_nn.py
@@ -18,6 +18,191 @@ import argparse
 import torch.nn as nn
 import torch.nn.functional as F
 
+'''
+Load the parameters of a NN from filename
+
+Model Properties:
+    Name
+    Training data
+    Testing data
+    Learning rate
+Weights and Biases
+'''
+
+'''
+Parse json string from file into python dict
+'''
+def read_json(filename):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+        return data
+
+'''
+Write json string to file from python dict
+'''
+def write_json(filename, data={}):
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+
+class Net(nn.Module):
+
+    def __init__(self, N=[1000, 100, 10, 1]):
+        super(Net, self).__init__()
+
+        # Create three fully connected layers, two of which are hidden and the third is
+        # the output layer.  In each case, the first argument o Linear is the number
+        # of input values from the previous layer, and the second argument is the number
+        # of nodes in this layer.  The call to the Linear initializer creates a PyTorch
+        # functional that in turn adds a weight matrix and a bias vector to the list of
+        # (learnable) parameters stored with each Net object.  These weight matrices
+        # and bias vectors are implicitly initialized using a normal distribution
+        # with mean 0 and variance 1
+        self.fc1 = nn.Linear(N[0], N[1], bias=True)
+        self.fc2 = nn.Linear(N[1], N[2], bias=True)
+        self.fc3 = nn.Linear(N[2], N[3], bias=True)
+
+    def forward(self, x):
+        #  The forward method takes an input Variable and creates a chain of Variables
+        #  from the layers of the network defined in the initializer. The F.relu is
+        #  a functional implementing the Rectified Linear activation function.
+        #  Notice that the output layer does not include the activation function.
+        #  As we will see, that is combined into the criterion for the loss function.
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class NN_Classifier:
+    def __init__(self, name=None, filename=None):
+        self.name = name
+        self.savepath = filename
+
+        self.curr_epoch = 0
+        self.learn_rate = 0.1
+
+        self.X_train = None
+        self.Y_train = None
+
+    def save_path(self, path):
+        self.savepath = path
+        self.name = os.path.splitext(os.path.basename(path))[0]
+        print(self.name)
+
+    def train_data(self, X, Y):
+        self.X_train = X
+        self.Y_train = Y
+
+    def init_net(self, len_nodes=[1000,100,10,1], learn_rate=.01) :
+        self.nodes = len_nodes
+        self.learn_rate = learn_rate
+        self.model = Net(len_nodes).cuda()
+        self.criterion = nn.CrossEntropyLoss().cuda()
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=learn_rate)
+
+    def load(self, filename=None):
+        path = None
+
+        # Use the filename if its give, otherwise fallback to self.savepath
+        if (filename != None):
+            path = filename
+        else:
+            path = self.savepath
+
+        checkpoint = torch.load(path)
+        self.nodes = checkpoint["nodes"]
+        self.curr_epoch = checkpoint["epoch"]
+        self.learn_rate = checkpoint["learn_rate"]
+        self.init_net(self.nodes, self.learn_rate)
+        self.model.load_state_dict(checkpoint["model_state_dict"])
+        self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        self.model.train()
+
+    def save(self, filename=None):
+        path = None
+
+        # Use the filename if its give, otherwise fallback to self.savepath
+        if (filename != None):
+            path = filename
+        else:
+            path = self.savepath
+
+        torch.save({
+                        'nodes': self.nodes,
+                        'epoch': self.curr_epoch,
+                        'learn_rate': self.learn_rate,
+                        'model_state_dict': self.model.state_dict(),
+                        'optimizer_state_dict': self.optimizer.state_dict(),
+                    }, path)
+
+        print("Model written to", path)
+
+    def train(self, batch_size=32):
+        print(self.learn_rate)
+
+        n_train = X_train.shape[0]
+        n_batches = int(np.ceil(n_train / batch_size))
+
+        print("Starting epoch",self.curr_epoch+1)
+        #  Create a random permutation of the indices of the row vectors.
+        indices = torch.randperm(n_train)
+
+        #  Run through each mini-batch
+        for b in range(n_batches):
+
+            #  Use slicing (of the pytorch Variable) to extract the
+            #  indices and then the data i/nstances for the next mini-batch
+            batch_indices = indices[b*batch_size: (b+1)*batch_size]
+            batch_X = X_train[batch_indices].cuda()
+            batch_Y = Y_train[batch_indices].cuda()
+
+            #  Run the network on each data instance in the minibatch
+            #  and then compute the object function value
+            pred_Y = self.model(batch_X)
+
+            loss = self.criterion(pred_Y, batch_Y)
+
+            #  Back-propagate the gradient through the network
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+        self.curr_epoch += 1
+
+    def model_accuracy(self, X, Y, batch_size=32):
+        n_train = X.shape[0]
+
+        n_batches = int(np.ceil(n_train / batch_size))
+
+        #  Create a random permutation of the indices of the row vectors.
+        indices = torch.randperm(n_train)
+
+        acc = 0
+        num_eq = 0
+        num_diff = 0
+        for b in range(n_batches):
+            #  Use slicing (of the pytorch Variable) to extract the
+            #  indices and then the data instances for the next mini-batch
+            batch_indices = indices[b*batch_size: (b+1)*batch_size]
+            batch_X = X[batch_indices].cuda()
+            batch_Y = Y[batch_indices].cuda()
+
+            #  Run the network on each data instance in the minibatch
+            #  and then compute the object function value
+            pred_Y = self.model(batch_X)
+            a, e, d = success_rate(pred_Y, batch_Y)
+            num_eq += e
+            num_diff += d
+        acc = num_eq / float(num_eq + num_diff)
+        # print('Training success rate:', acc)
+        return acc
+
+    def confusion_matrix(self, X, Y):
+        pred_Y = torch.max(self.model(X.cuda()), 1)[1].detach().cpu().numpy()
+        conf_matrix = metrics.confusion_matrix(pred_Y, Y)
+        return conf_matrix
+
+
 def success_rate(pred_Y, Y):
     _,pred_Y_index = torch.max(pred_Y, 1)
     num_equal = torch.sum(pred_Y_index.data == Y.data).item()
@@ -31,186 +216,162 @@ def read_json(filename):
         return data
 
 def load_data(filename):
-    print("Reading training images...")
+    print("Reading training images...", end="", flush=True)
     data = read_json(filename)
+    print("\tDone!\n")
 
     print("Creating data...")
-    X = [[int(i) for i in d] for d in data["X"]]
+    X_train = [[int(i) for i in d] for d in data["train"]["X"]]
+    X_test  = [[int(i) for i in d] for d in data["test" ]["X"]]
+    X_valid = [[int(i) for i in d] for d in data["valid"]["X"]]
 
-    c2l = []
-    l2c = {}
-    for cl in data["Y"]:
+    X_train = torch.Tensor(np.array(X_train).astype(np.uint8))
+    X_test  = torch.Tensor(np.array(X_test).astype(np.uint8))
+    X_valid = torch.Tensor(np.array(X_valid).astype(np.uint8))
+
+    c2l = []    # Class number to label
+    l2c = {}    # Label to class number
+
+    for cl in data["train"]["Y"]:
         if cl not in c2l:
             c2l.append(cl)
             l2c[cl] = len(c2l)
 
-    Y = [l2c[d] for d in data["Y"]]
+    Y_train = [l2c[d] for d in data["train"]["Y"]]
+    Y_test  = [l2c[d] for d in data["test"]["Y"]]
+    Y_valid = [l2c[d] for d in data["valid"]["Y"]]
 
-    return X, Y
+    Y_train = Variable(torch.Tensor.long(torch.Tensor(np.array(Y_train))))
+    Y_test = Variable(torch.Tensor.long(torch.Tensor(np.array(Y_test))))
+    Y_valid = Variable(torch.Tensor.long(torch.Tensor(np.array(Y_valid))))
 
-cli = argparse.ArgumentParser()
-cli.add_argument("--train", type=str)
-cli.add_argument("--test", type=str)
+    return (X_train, Y_train), (X_test, Y_test), (X_valid, Y_valid)
 
-args = cli.parse_args()
+'''
+TTV short for Train, Test, Validation
+'''
+def split_data_ttv(X):
+    tr = 0.15
+    te = 0.75
+    va = 0.15
 
-print("Training directories", args.train)
-print("Testing directories", args.test)
+    len_data = X.shape[0]
 
-train_dirs = args.train
-test_dirs = args.test
+    indices = torch.randperm(len_data)
+    X1 = X[indices]
 
-X_1, Y_1 = load_data(train_dirs)
-X_2, Y_2 = load_data(test_dirs)
+    i_1 = int(len_data*tr)
+    i_2 = int(len_data*(tr+te))
 
-X_train = torch.Tensor(np.array(X_1).astype(np.uint8))
-Y_train = Variable(torch.Tensor.long(torch.Tensor(np.array(Y_1))))
+    X_tr = X1[0:i_1]
+    X_te = X1[i_1:i_2]
+    X_va = X1[i_2:]
 
-X_test = torch.Tensor(np.vstack(X_2).astype(np.uint8))
-Y_test = Variable(torch.Tensor.long(torch.Tensor(np.array(Y_2))))
+    return X_tr, X_te, X_va
 
-n_train, n_test = X_train.shape[0], X_test.shape[0]
-#n_train, n_valid, n_test = 100, 100, 100
+#classifier.save("net_test.pt")
+if __name__ == "__main__":
+    # Define arguments
+    cli = argparse.ArgumentParser()
+    cli.add_argument("--new_classifier", type=str)
+    cli.add_argument("--load_classifier", type=str)
+    cli.add_argument("--train", type=str)
+    cli.add_argument("--test", type=str)
+    cli.add_argument("--batch_size", type=float, default=512)
+    cli.add_argument("--epochs", type=int, default=10)
+    cli.add_argument("--lr", type=float)
+    cli.add_argument("--stats", type=str)
 
-'''  Create network  '''
-N0 = X_train.shape[1]
-N1 = 250
-N2 = 100
-Nout = 52
-
-class Net(nn.Module):
-
-    def __init__(self):
-        super(Net, self).__init__()
-
-        # Create three fully connected layers, two of which are hidden and the third is
-        # the output layer.  In each case, the first argument o Linear is the number
-        # of input values from the previous layer, and the second argument is the number
-        # of nodes in this layer.  The call to the Linear initializer creates a PyTorch
-        # functional that in turn adds a weight matrix and a bias vector to the list of
-        # (learnable) parameters stored with each Net object.  These weight matrices
-        # and bias vectors are implicitly initialized using a normal distribution
-        # with mean 0 and variance 1
-        self.fc1 = nn.Linear(N0, N1, bias=True)
-        self.fc2 = nn.Linear(N1, N2, bias=True)
-        self.fc3 = nn.Linear(N2, Nout, bias=True)
-
-    def forward(self, x):
-        #  The forward method takes an input Variable and creates a chain of Variables
-        #  from the layers of the network defined in the initializer. The F.relu is
-        #  a functional implementing the Rectified Linear activation function.
-        #  Notice that the output layer does not include the activation function.
-        #  As we will see, that is combined into the criterion for the loss function.
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-#  Create an instance of this network.
-net = Net().cuda()
-criterion = nn.CrossEntropyLoss().cuda()
-optimizer = torch.optim.SGD(net.parameters(), lr=.0001)
-
-#  Print a summary of the network.  Notice that this only shows the layers
-print(net)
-
-#  Write the network weights to a file
-params = list(net.parameters())
-# f = open("nn_weights.txt", "w+")
-# for p in params:
-#     f.write(p.size())
-print(params[0].size()) # The parameter holding the layer 1 weight matrix
-print(params[1].size()) # ... the layer 1 bias vector
-print(params[2].size()) # ... the layer 2 weight matrix
-print(params[3].size()) # ... the layer 3 bias vector
-print(params[4].size()) # ... the layer 4 weight vector
-print(params[5].size()) # ... the layer 5 bias vector
-
-'''  Training  '''
-#  Set parameters to control the process
-epochs = 1000
-batch_size = 64
-n_batches = int(np.ceil(n_train / batch_size))
-
-# Store Test/Training accuracy per epoch
-train_acc = []
-test_acc = []
-
-for ep in range(epochs):
-    print("Starting epoch",ep+1)
-    #  Create a random permutation of the indices of the row vectors.
-    indices = torch.randperm(n_train)
-
-    #  Run through each mini-batch
-    for b in range(n_batches):
-        #  Use slicing (of the pytorch Variable) to extract the
-        #  indices and then the data instances for the next mini-batch
-        batch_indices = indices[b*batch_size: (b+1)*batch_size]
-        batch_X = X_train[batch_indices].cuda()
-        batch_Y = Y_train[batch_indices].cuda()
-
-        #  Run the network on each data instance in the minibatch
-        #  and then compute the object function value
-        pred_Y = net(batch_X)
-
-        loss = criterion(pred_Y, batch_Y)
-
-        #  Back-propagate the gradient through the network using the
-        #  implicitly defined backward function, but zero out the
-        #  gradient first.
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    acc = 0
-    num_eq = 0
-    num_diff = 0
-    for b in range(n_batches):
-        #  Use slicing (of the pytorch Variable) to extract the
-        #  indices and then the data instances for the next mini-batch
-        batch_indices = indices[b*batch_size: (b+1)*batch_size]
-        batch_X = X_train[batch_indices].cuda()
-        batch_Y = Y_train[batch_indices].cuda()
-
-        #  Run the network on each data instance in the minibatch
-        #  and then compute the object function value
-        pred_Y = net(batch_X)
-        a, e, d = success_rate(pred_Y, batch_Y)
-        num_eq += e
-        num_diff += d
-    acc = num_eq / float(num_eq + num_diff)
-    print('Training success rate:', acc)
-    train_acc.append(acc)
-
-    indices = torch.randperm(n_test)
-    for b in range(int(np.ceil(n_test / batch_size))):
-        #  Use slicing (of the pytorch Variable) to extract the
-        #  indices and then the data instances for the next mini-batch
-        batch_indices = indices[b*batch_size: (b+1)*batch_size]
-        batch_X = X_test[batch_indices].cuda()
-        batch_Y = Y_test[batch_indices].cuda()
-
-        #  Run the network on each data instance in the minibatch
-        #  and then compute the object function value
-        pred_Y = net(batch_X)
-        a, e, d = success_rate(pred_Y, batch_Y)
-        num_eq += e
-        num_diff += d
-
-    #  Compute and print the training and test loss
-    acc = num_eq / float(num_eq + num_diff)
-    print('Test success rate:', acc)
-    test_acc.append(acc)
+    # Parse args string into args object
+    args = cli.parse_args()
 
 
-pred_Y = torch.max(net(X_test.cuda()),1)[1].detach().cpu().numpy()
-print(pred_Y)
-print(Y_test)
+    train_dirs = args.train
+    test_dirs = args.test
 
-conf_matrix = metrics.confusion_matrix(pred_Y, Y_test)
-for l in conf_matrix.tolist():
-    print(l)
+    train, test, valid = load_data(train_dirs)
 
-plt.plot(train_acc, color="green")
-plt.plot(test_acc, color="red")
-plt.show()
+    X_train, Y_train = train
+    X_test, Y_test = test
+    X_valid, Y_valid = valid
+
+    #
+    # X_test = torch.Tensor(np.vstack(X_2).astype(np.uint8))
+    # Y_test = Variable(torch.Tensor.long(torch.Tensor(np.array(Y_2))))
+    # params[5].size()) # ... the layer 5 bias vector
+    #
+
+    classifier = NN_Classifier()
+
+    # Create a new classifier
+    if (args.new_classifier != None):
+        lr = args.lr if args.lr != None else 0.1
+        classifier.init_net(len_nodes=[X_train.shape[1],250,100,26], learn_rate=lr)
+        classifier.save_path(args.new_classifier)
+        classifier.save()
+    # Load an existing classifier from file
+    elif (args.load_classifier != None and os.path.isfile(args.load_classifier)):
+        classifier.save_path(args.load_classifier)
+        classifier.load()
+        if args.lr != None:
+            classifier.learn_rate = args.lr
+
+    stats = {}
+    if (args.stats != None):
+        if (os.path.isfile(args.stats)):
+            data = read_json(args.stats)
+            if ("name" in data and data["name"]==classifier.name and "epoch" in data and data["epoch"]==classifier.curr_epoch):
+                stats = data
+            else:
+                stats = {
+                    "name": classifier.name,
+                    "epoch": classifier.curr_epoch,
+                    "test_acc": [],
+                    "train_acc": [],
+                    "valid_acc": [],
+                    "confusion_matrix": []
+                }
+        else:
+            stats = {
+                "name": classifier.name,
+                "epoch": classifier.curr_epoch,
+                "test_acc": [],
+                "train_acc": [],
+                "valid_acc": [],
+                "confusion_matrix": []
+            }
+    '''  Training  '''
+    for i in range(100):
+        classifier.train(batch_size=512)
+        classifier.save()
+
+        tr_a = classifier.model_accuracy(X_train, Y_train, batch_size=512)*100
+        if ("train_acc" in stats):
+            stats["train_acc"].append(tr_a)
+        print("Train Accuracy: %3.1f" % tr_a)
+
+        va_a = classifier.model_accuracy(X_valid, Y_valid, batch_size=512)
+        if ("valid_acc" in stats):
+            stats["valid_acc"].append(va_a)
+        print("Validation Accuracy: %3.1f" % va_a)
+
+        if ("confusion_matrix" in stats):
+            stats["confusion_matrix"] = classifier.confusion_matrix(X_test, Y_test).tolist()
+
+        if ("epoch" in stats):
+            stats["epoch"] = classifier.curr_epoch
+
+        # Write stats to file
+        if (args.stats != None):
+            write_json(args.stats, stats)
+            print("Stats written to", args.stats)
+
+
+
+    plt.matshow(classifier.confusion_matrix(X_test, Y_test))
+    plt.show()
+    print("Test Accuracy:", classifier.model_accuracy(X_test, Y_test))
+
+    # plt.plot(train_acc, color="green")
+    # plt.plot(test_acc, color="red")
+    # plt.show()
